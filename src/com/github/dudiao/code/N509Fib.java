8b774e9db1bd9848112a4f6dc62028a9c76dd2ed
songyinyin <song_yinyin@163.com>
2020-12-02 23:03:34 +0800

    斐波那契数

---
+++ src/com/github/dudiao/code/N509Fib.java
@@ -0,0 +1,25 @@
+package com.github.dudiao.code;
+
+/**
+ * 斐波那契数
+ *
+ * @author songyinyin
+ * @date 2020/12/2 下午 09:43
+ */
+public class N509Fib {
+
+    public static void main(String[] args) {
+        int n = 0;
+        System.out.println("斐波那契数，f(" + n + ") = " + fib(n));
+    }
+
+    public static int fib(int n) {
+        int[] arr = new int[n + 1];
+        arr[0] = 0;
+        arr[1] = 1;
+        for (int i = 0; i <= n - 2; i++) {
+            arr[i + 2] = arr[i] + arr[i + 1];
+        }
+        return arr[n];
+    }
+}
